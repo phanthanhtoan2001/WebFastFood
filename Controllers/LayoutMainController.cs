fc77247b97946eaaec696dbc049fd05c0a855dea
DarkHero252 <101544268+DarkHero252@users.noreply.github.com>
2022-03-31 23:44:08 +0700

    Add files via upload

---
+++ Controllers/LayoutMainController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VSLT_FastfoodTeam.Controllers
+{
+    public class LayoutMainController : Controller
+    {
+        // GET: LayoutMain
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
