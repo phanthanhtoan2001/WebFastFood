7a9a57e9ded309796ccb61f838fbafa0d2ec03fd
phanthanhtoan2001 <101544256+phanthanhtoan2001@users.noreply.github.com>
2022-04-05 17:30:51 +0700

    Add files via upload

---
+++ Controllers/QuanLiPhieuNhapController.cs
@@ -0,0 +1,87 @@
+﻿using FastFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FastFood.Controllers
+{
+    public class QuanLiPhieuNhapController : Controller
+    {
+        // GET: QuanLiPhieuNhap
+        MyDataDataContext db = new MyDataDataContext();
+        [HttpGet]
+        public ActionResult NhapHang()
+        {
+            ViewBag.MaNCC = db.NCCs;
+            ViewBag.ListSanPham = db.SanPhams;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult NhapHang(PhieuNhap model, IEnumerable<CTPN> lstModel)
+        {
+          
+            SanPham sp;
+            foreach (var item in lstModel)
+            {
+                sp = db.SanPhams.Single(n => n.MaSP == item.MaSP);
+                sp.Soluongton += item.Soluong;
+                item.Maphieu = model.Maphieu;
+            }
+            db.CTPNs.InsertOnSubmit((CTPN)lstModel);
+            db.SubmitChanges();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DSSPHetHang()
+        {
+            var lstSP = db.SanPhams.Where(n => n.Soluongton <= 0);
+            return View(lstSP);
+
+        }
+        [HttpGet]
+        public ActionResult NhapHangDon(int? id)
+        {
+            ViewBag.MaNCC = new SelectList(db.NCCs.OrderBy(n => n.TenNCC ), "MaNCC", "TenNCC");
+            if (id == null)
+            {
+                Response.StatusCode = 404;
+                return null;
+            }
+            SanPham sp = db.SanPhams.SingleOrDefault(n => n.MaSP == id);
+            if (sp == null)
+            {
+                return HttpNotFound();
+            }
+            return View(sp);
+
+        }
+        [HttpPost]
+        public ActionResult NhapHangDon(PhieuNhap model, CTPN ctpn)
+        {
+            ViewBag.MaNCC = new SelectList(db.NCCs.OrderBy(n => n.TenNCC), "MaNCC", "TenNCC");
+            model.Ngaynhap= DateTime.Now;
+            
+            db.PhieuNhaps.InsertOnSubmit(model);
+            db.SubmitChanges();
+            ctpn.Maphieu = model.Maphieu;
+            SanPham sp = db.SanPhams.Single(n => n.MaSP == ctpn.MaSP);
+            sp.Soluongton += ctpn.Soluong;
+            db.CTPNs.InsertOnSubmit(ctpn);
+            db.SubmitChanges();
+            return View(sp);
+
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (db != null)
+                    db.Dispose();
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/SanPhamController.cs
@@ -0,0 +1,116 @@
+﻿using FastFood.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FastFood.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        // GET: SanPham
+       
+        MyDataDataContext db = new MyDataDataContext();
+        public ActionResult Listsanpham()
+        {
+            var all_food = from tt in db.SanPhams  orderby tt.Dongia ascending select tt;
+            return View(all_food);
+        }
+        public ActionResult Detail(int id)
+        {
+            var D_theloai = db.SanPhams.Where(m => m.MaSP == id).First();
+            return View(D_theloai);
+        }
+        //-------------Create-------------------
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(FormCollection collection, SanPham tl)
+        {
+            var masp = collection["MaSP"];
+            var tensp = collection["Tensp"];
+            var dongia = collection["Dongia"];
+            var size = collection["Size"];
+            var hinhanh = collection["Hinhanh"];
+            var soluongton = collection["Soluongton"];
+            var Mansx = collection["MaNSX"];
+            var Mancc = collection["MaNCC"];
+            var MaLoaisp = collection["MaLoaiSP"];
+            if (string.IsNullOrEmpty(tensp))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                tl.MaSP = int.Parse(masp);
+                tl.Tensp = tensp;
+                tl.Dongia = int.Parse(dongia);
+                tl.Size = size;
+                tl.Hinhanh = hinhanh;
+                tl.Soluongton = int.Parse(soluongton);
+                tl.MaNCC = Mancc;
+                tl.MaLoaiSP = int.Parse(MaLoaisp);
+                db.SanPhams.InsertOnSubmit(tl);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Create();
+        }
+        //-------------Edit-------------------
+        public ActionResult Edit(int id)
+        {
+            var E_category = db.SanPhams.First(m => m.MaSP == id);
+            return View(E_category);
+        }
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            var tl = db.SanPhams.First(m => m.MaSP == id);
+            var masp = collection["MaSP"];
+            var tensp = collection["Tensp"];
+            var dongia = collection["Dongia"];
+            var size = collection["Size"];
+            var hinhanh = collection["Hinhanh"];
+            var soluongton = collection["Soluongton"];
+            var Mansx = collection["MaNSX"];
+            var Mancc = collection["MaNCC"];
+            var MaLoaisp = collection["MaLoaiSP"];
+            if (string.IsNullOrEmpty(masp) || string.IsNullOrEmpty(tensp) || string.IsNullOrEmpty(dongia) || string.IsNullOrEmpty(size) || string.IsNullOrEmpty(hinhanh) || string.IsNullOrEmpty(soluongton) || string.IsNullOrEmpty(Mansx) || string.IsNullOrEmpty(Mancc) || string.IsNullOrEmpty(MaLoaisp))
+            {
+                ViewData["Error"] = "Don't empty!";
+            }
+            else
+            {
+                tl.MaSP = int.Parse(masp);
+                tl.Tensp = tensp;
+                tl.Dongia = int.Parse(dongia);
+                tl.Size = size;
+                tl.Hinhanh = hinhanh;
+                tl.Soluongton = int.Parse(soluongton);
+                tl.MaNCC = Mancc;
+                tl.MaLoaiSP = int.Parse(MaLoaisp);
+                UpdateModel(tl);
+                db.SubmitChanges();
+                return RedirectToAction("Index");
+            }
+            return this.Edit(id);
+        }
+        //-------------Delete-------------------
+        public ActionResult Delete(int id)
+        {
+            var D_theloai = db.SanPhams.First(m => m.MaSP == id);
+            return View(D_theloai);
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            var D_theloai = db.SanPhams.Where(m => m.MaSP == id).First();
+            db.SanPhams.DeleteOnSubmit(D_theloai);
+            db.SubmitChanges();
+            return RedirectToAction("ListSinhVien");
+        }
+    }
+}
